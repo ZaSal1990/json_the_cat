8a98254b9dbf48d33f31833a1aa2d3838161a0c5
ZaSal1990 <khan_zahra4424@hotmail.com>
2022-04-24 17:54:49 -0600

    introducing callback in main function while moving the whole functionality to the index file

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,12 +1,9 @@
 const request = require('request');
 
-const breedFetcher = function() {
-  let breed = process.argv[2];
-  request(`https://api.thecatapi.com/v1/breeds/search?q=` + breed, (error, body) => {
-    console.log(error);
-    const newData = JSON.parse(body.body);
-    console.log(newData[0].description);
+const fetchBreedDescription = function(breed, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=` + breed, (error, data) => {
+    callback(error, data);
   });
 };
 
-breedFetcher();
+module.exports = { fetchBreedDescription };
---
+++ index.js
@@ -0,0 +1,13 @@
+const { fetchBreedDescription } = require('./breedFetcher');
+
+const breedName = process.argv[2];
+
+fetchBreedDescription(breedName, (error, desc) => {
+  if (error) {
+    console.log('Error fetch details:', error);
+  } else {
+    const newData = JSON.parse(desc.body);
+    console.log(newData[0].description);
+
+  }
+});
